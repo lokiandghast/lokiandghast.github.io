a0e432a379b133489b9145d2fe72b6cc558f40cb
lokiandghast <115352378+lokiandghast@users.noreply.github.com>
2022-12-06 18:19:51 +0000

    insert shit here

---
+++ e/gdps/index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <!-- CSS Code -->
+<style type="text/css">
+.generatedImage { 
+    width:16px;
+    height:16px;
+    border-width:0px;
+    border-color:;
+    border-style:solid;
+    border-radius:px;
+    float:inherit;
+}
+ </style>
+    <!-- HTML Code -->
+        <meta charset="utf-8">
+        <title>MicroDeciml&#8482;, The Random Social Network!</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="canonical" href="http://html5-templates.com/" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" href="../style.css">
+        <script src="../js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+		<div class="wrapAll clearfix">
+			<div class="sidebar">
+				<div class="logo">
+					<a href="../"><img src='../img/logo.png' alt="logo"></a>
+				</div>
+				<div class="navigation">
+					<ul>
+						<li><a href="/contactus">contact us!<img   src="../img/contactus.png" alt="help" class="generatedImage"></a></li>
+						<li><a href="../credits">credits</a></li>
+						<li>> GDPS <img   src="../img/gdps.png" alt="gdps" class="generatedImage"></a></li>
+						<li><a href="../help">help <img   src="../img/help.png" alt="help" class="generatedImage"></a></li>
+						<li><a href="proxy.microdeci.ml">proxy <img   src="../img/proxy.png" alt="proxy" class="generatedImage"></a></li>
+						
+					</ul>
+					<h3>Interaction</h3>
+					<ul>
+						<li><a href="#">Help</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Portal</a></li>
+					</ul>
+				</div>
+
+			
+			</div>
+			<div class="mainsection">
+				<div class="headerLinks">
+				
+				</div>
+				<div class="tabs clearfix">
+					<div class="tabsLeft">
+						<ul>
+
+    <h1>GDPS</h1>
+    <p class="siteSub">loki's version</p>
+    <p class="roleNote">warning: this is <strong>NOT</strong> the source code. for the source code, go <a href="">here.</a></p>
+    <div class="articleRight">
+						<div class="contentsHeader">Downloads</div>
+						  <p><a href=./windows>  download for windows (EXE)	<img   src="../img/exe.png" alt="help" class="generatedImage"></a></p>
+						  <p>  <a href="./windowsmsi">  download for windows(MSI)	<img   src="../img/msi.png" alt="help" class="generatedImage"></a></p>
+						  <p> <a href="./android"> download for android (APK)	<img   src="../img/msi.png" alt="help" class="generatedImage"></a>
+					</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ e/index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <!-- CSS Code -->
+<style type="text/css">
+.generatedImage { 
+    width:16px;
+    height:16px;
+    border-width:0px;
+    border-color:;
+    border-style:solid;
+    border-radius:px;
+    float:inherit;
+}
+ </style>
+    <!-- HTML Code -->
+        <meta charset="utf-8">
+        <title>MicroDeci.ml&#8482;, The Random Social Network!</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="canonical" href="http://html5-templates.com/" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" href="style.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+		<div class="wrapAll clearfix">
+			<div class="sidebar">
+				<div class="logo">
+					<a href="../"><img src='img/logo.png' alt="logo"></a>
+				</div>
+				<div class="navigation">
+					<ul>
+						<li><a href="/contactus">contact us!<img   src="../img/contactus.png" alt="help" class="generatedImage"></a></li>
+						<li><a href="../credits">credits</a></li>
+						<li><a href="../gdps">GDPS <img   src="../img/gdps.png" alt="gdps" class="generatedImage"></a></li>
+						<li><a href="../help">help <img   src="../img/help.png" alt="help" class="generatedImage"></a></li>
+						<li><a href="proxy.microdeci.ml">proxy <img   src="../img/proxy.png" alt="proxy" class="generatedImage"></a></li>
+						
+					</ul>
+					<h3>Account</h3>
+					<ul>
+						<li><a href="../dashboard/login.php">login <img   src="../img/login.png" alt="proxy" class="generatedImage"></a></li></a></li>
+						<li><a href="../dashboard/join.php">sign up<img   src="../img/join.png" alt="proxy" class="generatedImage"></a></li></a></li>
+					</ul>
+				</div>
+
+			
+				</div>
+			<div class="mainsection">
+				<div class="headerLinks">
+				
+				</div>
+				<div class="tabs clearfix">
+					<div class="tabsLeft">
+						<ul>
+					<div class="tabsLeft">
+						<ul>
+							<li><a href=".." class="active">about us</a></li>
+							<li><a href="../tipsandtricks">tips and tricks</a></li>						
+						</ul>
+					
+						<h1> welcome to our site !</h1>
+						<p> if you are wondering, the reason this looks like a wikipedia page is because we used a free template.</p>
+					<div class="pagefooter">
+					<p> this website was made by <a href="https://team.microdeci.ml">the micro&#8482; team.</a> ...and a round of applause to the people that made this possible!!!</p>
+					<div class="footerlinks">
+					   <a href="https://www.000webhost.com"> <img   src="../img/000wh-pill.png" alt="thanks, 000webhost! <3" class="sticker"></a> | <a href="https://www.cloudflare.com"> <img   src="../img/cf-pill.png" alt="thanks, cloudflare! <3" class="sticker"></a> | <a href="https://www.freenom.com"><img   src="../img/fn-pill.png" alt="thanks, freenom! <3" class="sticker"></a> | <a href="https://www.humanstxt.org"><img   src="../img/h-txt-pill.png" alt="thanks, humans.txt! <3" class="sticker">
+					    
+					    
